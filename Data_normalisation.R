9286068d8066b2ac2b2829f9bc8a978c1498be10
Mari.H <Hambardzumyan@stud.uni-heidelberg.de>
2019-05-29 15:42:22 +0200

    Update Data_normalisation.R

--- Data_normalisation.R
+++ Data_normalisation.R
@@ -7,14 +7,14 @@ healthy_m_values <- log2(healthy_beta_values/(1- healthy_beta_values))
 
 # changing the ending of patients names from .bed to .M for better overview
 
-# changing cancer patients names
+# changing healthy patients names
 names(healthy_m_values)[names(healthy_m_values) == "Bcell_naive_VB_NBC_NC11_41.bed"] <- "Bcell_naive_VB_NBC_NC11_41.M"
 names(healthy_m_values)[names(healthy_m_values) == "Bcell_naive_VB_NBC_NC11_83.bed"] <- "Bcell_naive_VB_NBC_NC11_83.M"
 names(healthy_m_values)[names(healthy_m_values) == "Bcell_naive_VB_S001JP51.bed"] <- "Bcell_naive_VB_S001JP51.M"
 names(healthy_m_values)[names(healthy_m_values) == "Bcell_naive_VB_S00DM851.bed"] <- "Bcell_naive_VB_S00DM851.M"
 names(healthy_m_values)[names(healthy_m_values) == "Bcell_naive_VB_S01ECGA1.bed"] <- "Bcell_naive_VB_S01ECGA1.M"
 
-# changing healthy patients names
+# changing cancer patients names
 
 names(cancer_m_values)[names(cancer_m_values) == "cancer_VB_S01FE8A1.bed"] <- "cancer_VB_S01FE8A1.M"
 names(cancer_m_values)[names(cancer_m_values) == "cancer_VB_S01FF6A1.bed"] <- "cancer_VB_S01FF6A1.M"
