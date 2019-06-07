f22176e1dbb53f4d75174d6f64cc15f1abdbb12c
Pascal Lafrenz <pascal.lafrenz@gmx.de>
2019-06-07 15:34:07 +0200

    adjusting plots for better interpretation and visualization

--- Data_normalisation.R
+++ Data_normalisation.R
@@ -1,5 +1,6 @@
 
 
+
 # transforming beta values to M values and creating a separate dataframe with those values
 
 cancer_m_values <-
@@ -98,10 +99,15 @@ complete_cancer_m_values <-
 complete_healthy_m_values <-
   cbind.data.frame(healthy_m_values, mean_healthy_m_values, sd_healthy_m_values)
 
-#showing mean cancer m-values vs. mean healthy m-values
+##showing mean cancer m-values vs. mean healthy m-values
+#at first we have to load packages for visualization
 install.packages("tidyverse")
 library(tidyverse)
+install.packages("ggrepel")
+library(ggrepel)
 
+#extracting values of important genes (defined earlier) therefore we can highlight important genes in diagram
+#cancer
 complete_cancer_m_values_gene <- complete_cancer_m_values[c(
   "ENSG00000176887",
   "ENSG00000185551",
@@ -123,8 +129,9 @@ complete_cancer_m_values_gene <- complete_cancer_m_values[c(
   "ENSG00000105825",
   "ENSG00000159445",
   "ENSG00000122691"
-), ]
+),]
 
+#healthy
 complete_healthy_m_values_gene <- complete_healthy_m_values[c(
   "ENSG00000176887",
   "ENSG00000185551",
@@ -146,8 +153,69 @@ complete_healthy_m_values_gene <- complete_healthy_m_values[c(
   "ENSG00000105825",
   "ENSG00000159445",
   "ENSG00000122691"
-), ]
+),]
 
+#adding names of important genes to our important gene data frame because it is easier to work with
+complete_cancer_m_values_gene <-
+  cbind(
+    complete_cancer_m_values_gene,
+    Important_genes = c(
+      "SOX11",
+      "NR2F2",
+      "p53",
+      "CCND1",
+      "AHR",
+      "ROBO1",
+      "SOX9",
+      "HOXA9",
+      "CDH1",
+      "CDC14B",
+      "FOXC1",
+      "G0S2",
+      "GPX3",
+      "LGALS3",
+      "MAL",
+      "NPTX2",
+      "PAX6",
+      "TFPI2",
+      "THEM4",
+      "TWIST1"
+    ) 
+  )
+
+#now we plot our data with following features: 
+#mean values of cancer and healthy (black),
+#highlighting important genes for MCL (red), 
+#showing  general trend of points(blue smoothed curve; because of overplotting), 
+#straight line through origin (y=x) for better interpreting
+
+#first general overview
+ggplot() +
+  geom_point(
+    mapping = aes(
+      x = complete_cancer_m_values$mean_cancer_m_values,
+      y = complete_healthy_m_values$mean_healthy_m_values
+    ),
+    na.rm = TRUE,
+    alpha = 1 / 10
+  ) +
+  geom_smooth(
+    mapping = aes(
+      x = complete_cancer_m_values$mean_cancer_m_values,
+      y = complete_healthy_m_values$mean_healthy_m_values
+    ),
+    na.rm = TRUE,
+    alpha = 1 / 10,
+    colour = "darkgrey"
+  ) +
+  labs(x = "Mean cancer m-values",
+       y = "Mean healthy m-values",
+       title = "Comparison of mean m-values") +
+  theme_bw() +
+  xlim(-12, 9) +
+  ylim(-12, 9) 
+
+#now detail visualization
 ggplot() +
   geom_point(
     mapping = aes(
@@ -165,20 +233,24 @@ ggplot() +
     colour = "red",
     size = 2,
   ) +
-  geom_smooth(
-    mapping = aes(
-      x = complete_cancer_m_values$mean_cancer_m_values,
-      y = complete_healthy_m_values$mean_healthy_m_values
+  geom_label_repel(
+    aes(
+      label = Important_genes,
+      x = complete_cancer_m_values_gene$mean_cancer_m_values,
+      y = complete_healthy_m_values_gene$mean_healthy_m_values
     ),
-    na.rm = TRUE,
-    alpha = 1 / 10
+    data = complete_cancer_m_values_gene,
+    point.padding = 0.5,
+    label.size = 0.1,
+    segment.colour = "cornflowerblue",
+    segment.alpha = 0.9
   ) +
   labs(x = "Mean cancer m-values",
        y = "Mean healthy m-values",
        title = "Comparison of mean m-values") +
   theme_bw() +
-  xlim(-12, 9) +
-  ylim(-12, 9) +
+  xlim(-5, 5) +
+  ylim(-5, 5) +
   geom_abline(
     mapping = NULL,
     data = NULL,
@@ -188,6 +260,22 @@ ggplot() +
   )
 
 #showing SD of cancer m-values vs. SD of healthy m-values
+#first again overall visualization
+ggplot() +
+  geom_point(
+    mapping = aes(
+      x = complete_cancer_m_values$sd_cancer_m_values,
+      y = complete_healthy_m_values$sd_healthy_m_values
+    ),
+    na.rm = TRUE,
+    alpha = 1 / 10
+  ) +
+  labs(x = "SD cancer m-values",
+       y = "SD healthy m-values",
+       title = "Comparison of SD of m-values") +
+  theme_bw()
+
+#now detail visualization
 ggplot() +
   geom_point(
     mapping = aes(
@@ -205,12 +293,27 @@ ggplot() +
     colour = "red",
     size = 2,
   ) +
+  geom_label_repel(
+    aes(
+      label = Important_genes,
+      x = complete_cancer_m_values_gene$sd_cancer_m_values,
+      y = complete_healthy_m_values_gene$sd_healthy_m_values
+    ),
+    data = complete_cancer_m_values_gene,
+    point.padding = 0.5,
+    label.size = 0.1,
+    segment.colour = "cornflowerblue",
+    segment.alpha = 0.9,
+    max.iter = 5000
+  ) +
   labs(x = "SD cancer m-values",
        y = "SD healthy m-values",
        title = "Comparison of SD of m-values") +
+  xlim(-1, 2.5) +
+  ylim(0, 1) +
   theme_bw()
 
-#comparing mean beta values and mean m values
+#comparing mean beta values and mean m values (for Rmarkdown: compare to literature and say if it was successfull)
 #cancer
 ggplot() +
   geom_point(
