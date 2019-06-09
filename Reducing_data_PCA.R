e1271e65bf067bc4a66a5d1a146bc7c7fb88586c
Pascal Lafrenz <pascal.lafrenz@gmx.de>
2019-06-09 13:55:34 +0200

    adding plot for visualization how much variance is explained by each component, optimal amount of clusters, seperation of PC's into clusters

--- Reducing_data_PCA.R
+++ Reducing_data_PCA.R
@@ -3,17 +3,25 @@
 function_inf <- function(x) {
   if (x == 1) {
     return(0.99999)
-  } else {return(x)}
+  } else {
+    return(x)
+  }
 }
 function_minus_inf <- function(x) {
   if (x == 0) {
     return(0.00001)
-  } else {return(x)}
+  } else {
+    return(x)
+  }
 }
-cancer_beta_values <- as.data.frame(apply(cancer_beta_values, MARGIN = c(1,2), FUN = function_inf))
-cancer_beta_values <- as.data.frame(apply(cancer_beta_values, MARGIN = c(1,2), FUN = function_minus_inf))
-healthy_beta_values <- as.data.frame(apply(healthy_beta_values, MARGIN = c(1,2), FUN = function_inf))
-healthy_beta_values <- as.data.frame(apply(healthy_beta_values, MARGIN = c(1,2), FUN = function_minus_inf))
+cancer_beta_values <-
+  as.data.frame(apply(cancer_beta_values, MARGIN = c(1, 2), FUN = function_inf))
+cancer_beta_values <-
+  as.data.frame(apply(cancer_beta_values, MARGIN = c(1, 2), FUN = function_minus_inf))
+healthy_beta_values <-
+  as.data.frame(apply(healthy_beta_values, MARGIN = c(1, 2), FUN = function_inf))
+healthy_beta_values <-
+  as.data.frame(apply(healthy_beta_values, MARGIN = c(1, 2), FUN = function_minus_inf))
 # transforming beta values to M values and creating a separate dataframe with those values
 
 cancer_m_values <-
@@ -53,30 +61,95 @@ names(cancer_m_values)[names(cancer_m_values) == "cancer_VB_S01FKXA1.bed"] <-
 #merging both m value dataframes (healthy and cancer) into one again for PCA
 complete_m_values <- cbind(healthy_m_values, cancer_m_values)
 View(complete_m_values)
- 
+
 #Apply PCA on data frame "complete_m_values" with all m values. For that, the matrix needs to be transposed first
 #(variables are scaled to have i) standard deviation one and ii) mean zero)
 complete_m_values.pca <- prcomp(t(complete_m_values))
 summary(complete_m_values.pca)
 
+#visualize pca variation
+plot(complete_m_values.pca, main = "Variance explained through every principal component", type = "l")
+
 #visualize pca
-plot(complete_m_values.pca$x[,1], complete_m_values.pca$x[,2])
+plot(complete_m_values.pca$x[, 1], complete_m_values.pca$x[, 2])
 
 
 #adding an extra column with the category of sample with which we can color the pc dots in a ggplot according to their sample group
-pcs_of_m_values <- data.frame(cbind(complete_m_values.pca$x, Samples = c("Control", "Control", "Control", "Control", "Control", "MCL", "MCL", "MCL", "MCL", "MCL")))
+pcs_of_m_values <-
+  data.frame(cbind(
+    complete_m_values.pca$x,
+    Samples = c(
+      "Control",
+      "Control",
+      "Control",
+      "Control",
+      "Control",
+      "MCL",
+      "MCL",
+      "MCL",
+      "MCL",
+      "MCL"
+    )
+  ))
 
 #generate a ggplot/scatterplot to visualize the Sample points in a coordinate system with x-axis = PC1 and y-axis = PC2
-p <- ggplot(pcs_of_m_values, aes(PC1,PC2, group=Samples)) +
-  geom_point (aes(shape=Samples, color=Samples), size=4)
-p + scale_colour_manual(values=c("seagreen2", "indianred1"))
+p <- ggplot(pcs_of_m_values, aes(PC1, PC2, group = Samples)) +
+  geom_point (aes(shape = Samples, color = Samples), size = 4)
+p + scale_colour_manual(values = c("seagreen2", "indianred1"))
 
 #finding the top 25 most important genes (with the biggest influence). Therefore we will look at the loading scores (saved in "rotation") of the genes on PC1. Because it's not important
 #whether it is positive or negative we will look at the absolute values and rank these
 
-loading_scores <- complete_m_values.pca$rotation[,1]
+loading_scores <- complete_m_values.pca$rotation[, 1]
 ranked_gene_loading <- sort(abs(loading_scores), decreasing = TRUE)
 top_25_genes <- names(ranked_gene_loading[1:25])
 View(top_25_genes)
 
-complete_m_values.pca$rotation[top_25_genes,1]
+complete_m_values.pca$rotation[top_25_genes, 1]
+
+#find out how much clusters do we need to group samples (obvisiously 2 would be perfekt because healthy/cancer)
+wss <-  sapply(1:5, function(k) {
+  kmeans(x = complete_m_values.pca$x,
+         centers = k,
+         iter.max = 100)$tot.withinss
+})
+plot(
+  1:5,
+  wss,
+  type = "b",
+  pch = 19,
+  xlab = "Number of clusters K",
+  ylab = "Total within-clusters sum of squares"
+)
+
+##--> it seems we need 2 clusters (kink in the curve)
+
+#find out if healthy and cancer samples are seperated
+#variable with two center positions of value of rotated data
+centers <-
+    kmeans(x = t(pcs_of_m_values[1:10]),
+           centers = 2,
+           iter.max = 100)$centers
+
+##adding an extra column with the category of sample with which we can color the pc dots in a ggplot according to their sample group
+centers <-
+  data.frame(cbind(
+    t(centers),
+    Samples = c(
+      "Control",
+      "Control",
+      "Control",
+      "Control",
+      "Control",
+      "MCL",
+      "MCL",
+      "MCL",
+      "MCL",
+      "MCL"
+    )
+  ))
+
+#visualize cluster x1 and x2 and how samples are seperated
+p_cluster <- ggplot(centers, aes(X1, X2, group = Samples)) +
+  geom_point (aes(shape = Samples, color = Samples), size = 4)
+p_cluster + scale_colour_manual(values = c("seagreen2", "indianred1"))
